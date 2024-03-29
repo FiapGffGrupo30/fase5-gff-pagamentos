fc163d9cfedfd099a2994e7105272f2d5629137b
Marcus <marcusvcl@gmail.com>
2024-03-29 10:17:26 -0300

    feat: fixing endpoint getById

--- src/main/java/br/fiap/gff/payments/application/controllers/ReceiptController.java
+++ src/main/java/br/fiap/gff/payments/application/controllers/ReceiptController.java
@@ -3,10 +3,7 @@ package br.fiap.gff.payments.application.controllers;
 
 import br.fiap.gff.payments.models.Receipt;
 import br.fiap.gff.payments.usecases.ReceiptUseCase;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +18,7 @@ public class ReceiptController {
 
     @GET
     @Path("/{id}")
-    public Response getById(String id) {
+    public Response getById(@PathParam("id") String id) {
         Receipt r = receipts.getById(id);
         return Response.ok(r).build();
     }
